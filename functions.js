f1d3dd319086c5ac7020008a48f3767e8da0f692
Lindsay <lindsay@netdreams.nl>
2025-10-09 16:29:05 +0200

    beginning of playGame function

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 let humanScore =  0;
 let computerScore  =  0;
+let totalHumanScore =  0;
+let totalComputerScore  =  0;
 
 /*
     create a function that returns the user input and 
@@ -29,7 +31,7 @@ function getHumanChoice(){
 
 /* 
     create a function that returns one of the following strings: 
-    rock - paper - scissor
+    rock - paper - scissors
 */
 function getComputerChoice(){
     // use math.random to return a random value between 0 - 2
@@ -61,42 +63,64 @@ function getComputerChoice(){
     log winner announcement
 */
 
-function playRound(humanchoice, computerChoice){
-    humanchoice = getHumanChoice();
+function playRound(humanChoice, computerChoice){
+    humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
 
     //declare a winner based on computer answer and human answer
-    if(computerChoice === 'paper' & humanchoice === 'rock'){
+    if(computerChoice === 'paper' && humanChoice === 'rock'){
         console.log('Computer wins, paper beats rock!');
         computerScore++;
         console.log(`Computer gets a point. ${computerScore}`);
-    } else if(computerChoice === 'rock' & humanchoice === 'paper'){
+    } else if(computerChoice === 'rock' && humanChoice === 'paper'){
         console.log('You win, paper beats rock!');
         humanScore++;
         console.log(`You get a point. ${humanScore}`);
-    } else if(computerChoice === 'rock' & humanchoice === 'rock'){
+    } else if(computerChoice === 'rock' && humanChoice === 'rock'){
         console.log('Its a tie! Try again.');
-    } else if(computerChoice === 'rock' & humanchoice === 'scissors'){
+    } else if(computerChoice === 'rock' && humanChoice === 'scissors'){
         console.log('Computer wins, rock beats scissors!');
-        humanScore++;
+        computerScore++;
         console.log(`Computer gets a point. ${computerScore}`);
-    } else if(computerChoice === 'scissors' & humanchoice === 'rock'){
+    } else if(computerChoice === 'scissors' && humanChoice === 'rock'){
         console.log('You win, rock beats scissors!');
         humanScore++;
         console.log(`You get a point. ${humanScore}`);
-    } else if(computerChoice === 'scissors' & humanchoice === 'scissors'){
+    } else if(computerChoice === 'scissors' && humanChoice === 'scissors'){
         console.log('Its a tie! Try again.');
-    } else if(computerChoice === 'scissors' & humanchoice === 'paper'){
+    } else if(computerChoice === 'scissors' && humanChoice === 'paper'){
         console.log('Computer wins, scissors beats paper!');
         computerScore++;
         console.log(`Computer gets a point. ${computerScore}`);
-    } else if(computerChoice === 'paper' & humanchoice === 'scissors'){
+    } else if(computerChoice === 'paper' && humanChoice === 'scissors'){
         console.log('You win, scissors beats paper!');
         humanScore++;
         console.log(`you get a point. ${humanScore}`);
-    } else if(computerChoice === 'paper' & humanchoice === 'paper'){
+    } else if(computerChoice === 'paper' && humanChoice === 'paper'){
         console.log('Its a tie! Try again.');
     }
+
+    return {
+        humanChoice: humanChoice,
+        computerChoice: computerChoice,
+        humanScore: humanScore,
+        computerScore: computerScore
+    };
 }
 
-playRound();
+
+/*
+    calls playRound() 5 times
+    declares a winner after 5 rounds based on total score results
+    After one round total score will be incremented 
+    Keeps track of player & computer score
+*/ 
+
+function playGame(){
+    let totalGame = 5;
+    for (round = 0; round < totalGame; round++){
+       console.log(`Started round ${round} of ${totalGame}`);
+       playRound();
+    }
+}
+playGame();
